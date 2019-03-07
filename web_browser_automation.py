232f1f29f4dc9fe80a623ca40f140b87d528ecb6
Shafeeq Rahiman <shafeeqtk@gmail.com>
2019-03-07 15:44:45 +0400

    added more lines of code

---
+++ web_browser_automation.py
@@ -0,0 +1,7 @@
+#TODO: Add code for Selenium automation
+
+from selenium import webdriver
+from selenium.webdriver import ActionChains
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.chrome.options import Options
+
