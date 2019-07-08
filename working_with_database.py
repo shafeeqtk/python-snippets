fe395804f1d4eaa66028667e313f0ed009528096
Shafeeq Rahiman <shafeeqtk@gmail.com>
2019-07-08 10:19:50 +0400

    Update working_with_database.py

--- working_with_database.py
+++ working_with_database.py
@@ -18,7 +18,7 @@ df = pd.read_sql('select * from schema_name."tablename" limit 10', con)
 '''
 WRITING TO THE DATABASE
 '''
-
+## data transfer
 #read csv file as chunks
 chunks = pd.read_csv("source.csv", chunksize=100000)
 
@@ -26,3 +26,16 @@ chunks = pd.read_csv("source.csv", chunksize=100000)
 for chunk in chunks:
     chunk.to_sql(schema='schema_name', name="dest_table", if_exists='replace', con=con, index=False)
 
+##Selective insert
+sql = '''insert into <tablename> (column1, column2, column3) 
+        values ( 
+         %s 
+          ,%s 
+          ,%s
+          )'''
+
+con_pc.execute(sql, (
+             'value1',
+             'value2',
+             'value3',
+             ))
